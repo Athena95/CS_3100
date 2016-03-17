d7a7e3090c7d47b337e60935fdf9e8a38f05afb3
Athena95 <email@krista.tel>
2016-03-16 22:45:46 -0400

    Revert "Clean up"
    This reverts commit 087aef306b71795c1ff8365ced7c7e6a3c79cdb9.

--- Project2/OrgTree/main.cpp
+++ Project2/OrgTree/main.cpp
@@ -2,13 +2,28 @@
 #include <iostream>
 
 int main() {
-
 	std::string filename = "employees.txt";
 
 	TreeOrg employees;
 	employees.read(filename);
 
 	// employees.addRoot("Boss Man", "Boss");
+
+	for (size_t i = 0; i < employees.getSize(); ++i) {
+
+		if (i == 0) {
+			std::cout << "Head honcho" <<
+				employees.tree_[i].name_ << "\n" << std::endl;
+			continue;
+		}
+
+// TODO- remove access to tree-only for temporary testing purposes
+		std::cout << "Employee: " <<
+			employees.tree_[i].name_ << std::endl;
+
+		std::cout << "Boss: " <<
+			employees.tree_[employees.tree_[i].parent_].name_ << "\n" << std::endl;
+	}
 
 	std::cout << "Press enter to continue . . . " << std::endl;
 	std::cin.get();
