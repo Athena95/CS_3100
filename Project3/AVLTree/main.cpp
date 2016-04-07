744be26dd6241f49271baf0ee76d1e0d4ec6661d
Athena95 <email@krista.tel>
2016-04-07 13:53:25 -0400

    Update main testing

--- Project3/AVLTree/main.cpp
+++ Project3/AVLTree/main.cpp
@@ -11,10 +11,16 @@ int main() {
 		tree.insert(i, i + 1);
 	}
 
-	tree.find(6, 50);
-	tree.findRange(15, 19);
-	tree.getSize();
+	tree.find(2, 2);
+	std::vector<int> rangeVector(tree.findRange(15, 19));
+	
+	for (auto it = rangeVector.begin(); it != rangeVector.end(); ++it) {
+		std::cout << *it << " " << std::endl;
+	}
+	
+	std::cout << tree.getSize() << std::endl;
 	tree.print();
 
+	system("pause");
 	return 0;
 }
