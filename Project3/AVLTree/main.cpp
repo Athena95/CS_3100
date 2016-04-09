4d4297bc50be623f0f73b7d3865d34e56c386965
Athena95 <email@krista.tel>
2016-04-09 13:21:34 -0400

    Update main
    More testing for stuff and things.

--- Project3/AVLTree/main.cpp
+++ Project3/AVLTree/main.cpp
@@ -7,12 +7,24 @@ int main() {
 	AVLTree tree;
 	size_t numItems = 20;
 
-	for (size_t i = 0; i < numItems; ++i) {
-		tree.insert(i, i + 1);
-	}
+	tree.insert(2, 5);
+	tree.insert(1, 700);
+	tree.insert(9, 13);
+	tree.insert(4, 10);
+	tree.insert(1, 80);
+	tree.insert(90, 110);
+	tree.insert(55, 30);
+	tree.insert(7, 83);
+	tree.insert(43, 18);
+	//tree.insert(1, 1);
+	//tree.insert(2, 2);
+	//tree.insert(3, 3);
 
-	tree.find(2, 2);
-	std::vector<int> rangeVector(tree.findRange(15, 19));
+	int myInt = 0;
+	tree.find(55, myInt);
+
+	std::cout << "Using key 55, I found: " << myInt << std::endl;
+	std::vector<int> rangeVector(tree.findRange(50, 100));
 	
 	for (auto it = rangeVector.begin(); it != rangeVector.end(); ++it) {
 		std::cout << *it << " " << std::endl;
@@ -20,6 +32,8 @@ int main() {
 	
 	std::cout << tree.getSize() << std::endl;
 	tree.print();
+
+	std::cout << tree.getHeight() << std::endl;
 
 	system("pause");
 	return 0;
